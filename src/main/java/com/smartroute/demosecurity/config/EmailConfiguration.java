4b86f6e576c43707cbe3776d7b5ffe221c759d77
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-12 13:09:16 -0500

    Update EmailConfiguration.java

--- src/main/java/com/smartroute/demosecurity/config/EmailConfiguration.java
+++ src/main/java/com/smartroute/demosecurity/config/EmailConfiguration.java
@@ -9,29 +9,28 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-
 public class EmailConfiguration {
-@Value("${spring.mail.username}")
-private String emailUsername;
-@Value("${spring.mail.password}")
-private String password;
 
+    @Value("${spring.mail.username}")
+    private String emailUsername;
 
-@Bean
-    public JavaMailSender javaMailSender(){
-    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-    mailSender.setUsername(emailUsername);
-    mailSender.setPassword(password);
+    @Value("${spring.mail.password}")
+    private String emailPassword;
 
-    Properties props = mailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(emailPassword);
 
-    return mailSender;
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
-
 }
