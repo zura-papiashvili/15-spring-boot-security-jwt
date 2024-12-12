18a96056dba5acb176ab88eef7652088ac08f053
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-12 17:58:42 -0500

    Update User.java

--- src/main/java/com/smartroute/demosecurity/model/User.java
+++ src/main/java/com/smartroute/demosecurity/model/User.java
@@ -49,6 +49,12 @@ public class User implements UserDetails{
     }
 
     @Override
+    public String getUsername() {
+
+        return email;
+    }
+
+    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
