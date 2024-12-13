2a9e2ce0f164b7df3de80698350153441d8de0c7
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-12 19:47:02 -0500

    Update AuthenticationController.java

--- src/main/java/com/smartroute/demosecurity/controller/AuthenticationController.java
+++ src/main/java/com/smartroute/demosecurity/controller/AuthenticationController.java
@@ -7,28 +7,27 @@ import com.smartroute.demosecurity.model.User;
 import com.smartroute.demosecurity.responses.LoginResponse;
 import com.smartroute.demosecurity.service.AuthenticationService;
 import com.smartroute.demosecurity.service.JwtService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/auth")
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final JwtService jwtService;
 
-    public AuthenticationController(AuthenticationService authenticationService, JwtService jwtService) {
-        this.authenticationService = authenticationService;
-        this.jwtService = jwtService;
-    }
-
     @PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto){
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
         User user = authenticationService.signup(registerUserDto);
         return ResponseEntity.ok(user);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto){
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
         User authenticatedUser = authenticationService.authenticate(loginUserDto);
         String jwtToken = jwtService.generateToken(authenticatedUser);
         LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
@@ -36,11 +35,11 @@ public class AuthenticationController {
     }
 
     @PostMapping("/verify")
-    public ResponseEntity<?> verify(@RequestBody VerifyUserDto verifyUserDto){
+    public ResponseEntity<?> verify(@RequestBody VerifyUserDto verifyUserDto) {
         try {
-             authenticationService.verifyUser(verifyUserDto);
+            authenticationService.verifyUser(verifyUserDto);
             return ResponseEntity.ok("User verified");
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
